4e34709534a7eff2799448513d3c54fa86db8a6a
Dave Ames <david.john.ames@gmail.com>
2023-12-02 17:57:55 +0000

    Slightly clunky solution to the second part

--- Day01/LineObject.py
+++ Day01/LineObject.py
@@ -1,6 +1,10 @@
 class LineObject:
     def __init__(self, line):
+        self.words_as_numbers = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+                                 "six": "6", "seven": "7", "eight": "8", "nine": "9"}
         self.line = line
+        self.registered_numbers = dict()
+        self.register_numeric_words()
         self.digits = []
         self.collect_digits()
         self.extracted_number = 0
@@ -8,15 +12,33 @@ class LineObject:
 
 
     def collect_digits(self):
-        for letter in self.line:
+        for index, letter in enumerate(self.line):
             if letter.isdigit():
                 self.digits.append(letter)
+            elif index in self.registered_numbers:
+                self.digits.append(self.registered_numbers[index])
 
     def generate_first_and_last_number(self):
         string_number = self.digits[0] + self.digits[-1]
         self.extracted_number = int(string_number)
 
+    def register_numeric_words(self):
+        for word in self.words_as_numbers:
+            if word in self.line:
+                word_positions = list(self.find_all(word))
+                for position in word_positions:
+                    self.registered_numbers[position] = self.words_as_numbers[word]
+
+    def find_all(self, word):
+        start = 0
+        while True:
+            start = self.line.find(word, start)
+            if start == -1:
+                return
+            yield start
+            start += 1
 
 if __name__ == "__main__":
-    test_1 = LineObject("this1isthe2")
-    test_2 = LineObject("hello7isn't")
+
+    test_1 = LineObject("twothreefour")
+    test_2 = LineObject("eightwoeight56")
---
+++ Day01/Part2.py
@@ -0,0 +1,7 @@
+from DocumentObject import DocumentObject
+
+# example = DocumentObject("example2.txt")
+# print(example.grand_total)
+
+solution = DocumentObject("input.txt")
+print(solution.grand_total)
