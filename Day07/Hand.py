f3730107521275c40cacc7fbcc6307ad32352e64
Dave Ames <david.john.ames@gmail.com>
2023-12-07 16:17:43 +0000

    Finished off Part 2 for Day 7

--- Day07/Hand.py
+++ Day07/Hand.py
@@ -5,7 +5,7 @@ class Hand:
             "A": 14,
             "K": 13,
             "Q": 12,
-            "J": 11,
+            "J": 1,
             "T": 10,
             "9": 9,
             "8": 8,
@@ -27,6 +27,8 @@ class Hand:
         }
         self.hand, self.bid = hand_data.split(" ")
         self.bid = int(self.bid)
+        self.tie_break_hand = self.hand
+        self.deal_with_jokers()
         self.hand_type = ""
         self.calculate_hand_type()
 
@@ -52,13 +54,29 @@ class Hand:
         elif number_of_distinct_cards == 1:
             self.hand_type = "Five of a Kind"
 
+    def deal_with_jokers(self):
+        if "J" in self.hand:
+            position_of_first_j = self.hand.rfind("J")
+            cards = collections.defaultdict(int)
+            for card in self.hand:
+                cards[card] += 1
+            number_of_distinct_cards = len(cards)
+            if number_of_distinct_cards == 1:
+                return
+            else:
+                del cards["J"]
+                card_name = max(cards, key = lambda key: cards[key])
+                self.hand = self.hand[:position_of_first_j] + card_name + self.hand[position_of_first_j+1:]
+            self.deal_with_jokers()
+
+
     def __lt__(self, other):
         if self.hand_type != other.hand_type:
             return self.hand_type_ordering[self.hand_type] < self.hand_type_ordering[other.hand_type]
         else:
-            for index in range(len(self.hand)):
-                if self.hand[index] != other.hand[index]:
-                    return self.card_ordering[self.hand[index]] < self.card_ordering[other.hand[index]]
+            for index in range(len(self.tie_break_hand)):
+                if self.tie_break_hand[index] != other.tie_break_hand[index]:
+                    return self.card_ordering[self.tie_break_hand[index]] < self.card_ordering[other.tie_break_hand[index]]
 
     def __gt__(self, other):
         return other < self
@@ -68,12 +86,16 @@ class Hand:
 
 
 if __name__ == "__main__":
-    hand1 = Hand("32T3K 765")
-    hand2 = Hand("T55J5 684")
-    hand3 = Hand("KK677 28")
-    hand4 = Hand("KTJJT 220")
-    hand5 = Hand("QQQJA 483")
-    hand6 = Hand("T44T4 23")
-    hand7 = Hand("T4444 23")
-    hand8 = Hand("T1234 23")
-    print(hand4 < hand5)
+    # hand1 = Hand("32T3K 765")
+    # hand2 = Hand("T55J5 684")
+    # hand3 = Hand("KK677 28")
+    # hand4 = Hand("KTJJT 220")
+    # hand5 = Hand("QQQJA 483")
+    # hand6 = Hand("T44T4 23")
+    # hand7 = Hand("T4444 23")
+    # hand8 = Hand("T1234 23")
+    # print(hand4 < hand5)
+    hand1 =  Hand("AAAJA 234")
+    print(hand1)
+    hand2 = Hand("JJJJA 234")
+    print(hand2)
